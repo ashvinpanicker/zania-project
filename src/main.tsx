d8f9c68c07a918c9601c89afea54988b31a9cbef
Ashvin Panicker <ashvin.panicker@gmail.com>
2024-11-21 03:22:05 +0530

    allow msw on vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 async function startApp() {
-  if (process.env.NODE_ENV === 'development') {
-    const { worker } = await import('./mocks/browser');
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.REACT_APP_USE_MSW === "true"
+  ) {
+    const { worker } = await import("./mocks/browser");
     // Initialize MSW
-    await worker.start({ 
-      onUnhandledRequest: 'bypass',
+    await worker.start({
+      onUnhandledRequest: "bypass",
       serviceWorker: {
-        url: '/mockServiceWorker.js'
-      }
+        url: "/mockServiceWorker.js",
+      },
     });
   }
 
-  createRoot(document.getElementById('root')!).render(
+  createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
     </StrictMode>
